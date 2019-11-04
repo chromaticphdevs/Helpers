43cab68806206feb8b3425516d69ba9d163e6960
Mark Angelo Resma Gonzales <gonzalesmarkangeloph@gmail.com>
2019-11-04 21:04:49 +0800

    Update File.php
    updated file helper shrink method updated and some method names have been modified

--- File.php
+++ File.php
@@ -1,124 +1,278 @@
-<?php
+<?php 	
 
-	class File
-	{	
-		private $_file;
-		private $_ok_file_extension = ['jpeg' , 'jpg' , 'png' , 'bitmap'];
-		private $_errors = array();
-		private $_dir;
+	class File 
+	{
+		/*Required*/
 
-		private $_prefix = '';
-		public function setFile($file = null)
+		private $_acceptedFileType = ['png' , 'jpeg' , 'jpg' , 'bitmap' , 'pdf' , 'docx' , 'doc' , 'xls'];
+
+		private $_errors = [] , $notes = [];
+
+		/*MAXIMUM FILE SIZE (KB TYPE)*/
+
+		private $_maxSize = 150; //300 kb
+		/*set the file*/
+		public function setFile($file)
 		{
-			try
-			{
-				if(!empty($file['error']))
-				{
-					throw new Exception("Error found on uploading");	
+			try{
+				if(!isset($file['tmp_name'])){
+					// $this->addError('File does not exists');
+					throw new Exception("File does not exists", 1);
 				}
-				if(empty($file['name']))
-				{
-					throw new Exception("No File Found");
-				}
-				else
-				{
-					$this->_file = $file;
-					$this->tmp_name = $file['tmp_name'];
-					$this->name = $file['name'];
-					
-					//Extract extension on file name;
-					$ext = explode('.', $this->name);
-					//set file extension
-					$this->ext = strtolower(end($ext));
 
-					return $this;
+				if(!isset($file['name'])){
+					// $this->addError('File does not exists');
+					throw new Exception("File does not exists", 1);
 				}
-			}catch(Exception $e)
-			{
-				echo $e->getMessage();
-				die();
+
+				$this->_fileTmp  = $file['tmp_name'];
+				$this->_fileName = $file['name'];
+				$this->_fileSize = $file['size'];
+				return $this;
+			}catch(Exception $e){
+
+				$this->addError($e->getMessage());
+			}finally{
+
+				return $this;
 			}
+			
 		}
 
+		/*set upload file*/
 		public function setDIR($dir)
 		{
 			$this->_dir = $dir;
+
 			return $this;
 		}
 
+		/*set name prefix*/
+		public function setPrefix($prefix)
+		{
+			$this->_prefix = $prefix;
+			return $this;
+		}
+
+		/*set custom name*/
+		public function setCustomName($customName)
+		{	
+			$this->_customName = $customName;
+			return $this;
+		}
+
+		/*add file type that can be accepted*/
+		public function addFileType($fileType)
+		{
+			if(is_array($fileType))
+			{
+				$difference = array_diff($fileType, $this->_acceptedFileType);
+
+				if(!empty($difference)){
+					array_merge($this->_acceptedFileType , $difference);
+				}
+			}else
+			{
+				array_push($this->_acceptedFileType, $fileType);
+			}
+
+			return $this;
+		}
+
+		/*alter file type extion accept*/
+		public function alterFileType(array $fileTypeList)
+		{
+			$this->_acceptedFileType = $fileTypeList;
+		}
+
+		/*trigger upload*/
 		public function upload()
 		{
-			if(is_null($this->_file))
-			{
-				$this->addErrors('No file uploaded');
+			if(! empty($this->errors)){
+				return [
+					'success' => false ,
+					'errors'  => implode(',', $this->_errors)
+				];
 			}else
 			{
-				//check if extension is valid
-				if($this->isValidExtension())
-				{
-					$this->uploadFile();
+				//upload file check extension
+				$getExtension = explode('.', $this->_fileName);
+
+				$extension    = strtolower(end($getExtension));
+
+				if(! in_array($extension, $this->_acceptedFileType)){
+
+					$this->addError("Invalid extension '{$extension}'");
 				}
-				
+
+				if(!empty($this->errors)){
+
+					return [
+						'success' => false,
+						'errors'  => implode(',', $this->_errors)
+					];
+				}else
+				{
+					//check if dir exits;
+					if(!property_exists($this, '_dir')){
+
+						$this->addError("Upload file directory is not set");
+
+						return [
+							'success' => false ,
+							'errors'  => implode(',' , $this->_errors)
+						];
+					}else{
+
+						$uploadName = $this->_customName ?? $this->makeName($extension);
+
+						$prefix  = $this->_prefix ?? '';
+
+						$newName = $this->_fileUploadName =  $prefix.''.$uploadName;
+
+						$pathUpload = $this->_pathUpload = $this->_dir.DIRECTORY_SEPARATOR.$newName;
+
+						//check if file is oversize
+						if( $this->isOverSize() ) {
+							//we will shrink the image and append it to the note array
+							// $this->addError("File is too large max size is '{$this->_maxSize}kB' but the system will resize it.");
+							$this->shrinkSize();
+
+							$this->addNotes("Your Image is too large. the system shrink it");
+						}else{
+							if(!move_uploaded_file($pathUpload)){
+								return [
+									'success' => false ,
+									'errors'  => implode(',' , $this->_errors)
+								];
+							}
+						}
+					}
+
+					return [
+						'success' => true
+					];
+				}
 			}
 		}
-		public function setPrefix($_prefix)
-		{
-			$this->_prefix = $_prefix;
-			return $this;
-		}
-		public function getNewName()
-		{
-			return $this->newname;
-		}
 
-		public function getName()
+		private function isOverSize($resize = true)
 		{
-			return $this->name;
-		}
-		private function isValidExtension()
-		{
-			if(in_array(strtolower($this->ext),$this->_ok_file_extension))
-			{
+			if($this->_maxSize < ($this->_fileSize / 100)){
 				return true;
-			}else
-			{
-				$this->addErrors('Invalid Extension : ' .$this->ext);
+			}else{
+				return false;
 			}
-			return false;
 		}
-		private function uploadFile()
+		/*auto set name*/
+		private function makeName($extension)
 		{
-			$newName = $this->generateName();
-			try
+			return strtolower(uniqid().'.'.$extension);
+		}
+
+		public function getFileUploadName()
+		{
+			return $this->_fileUploadName;
+		}
+		/*add errors*/
+		private function addError($err)
+		{
+			$this->_errors[] = $err;
+		}
+		public function getErrors()
+		{
+			return implode(',' , $this->_errors);
+		}
+
+		private function addNotes($notes)
+		{
+			$this->_notes [] = $notes;
+		}
+
+		public function getNotes()
+		{
+			return $this->_notes;
+		}
+
+		/*SHRINK*/
+
+		private function shrinkSize()
+		{
+			$fileTmp = $this->_fileTmp;
+			$fileName = $this->_fileName;
+			/*get image size*/
+			$sourceProperty = getimagesize($fileTmp);
+			/*get the image type*/
+			$image_type = $sourceProperty[2];
+
+			if( $image_type == IMAGETYPE_JPEG ) 
+			{   
+				$imageSourceId = imagecreatefromjpeg($fileTmp);  
+
+				$targetLayer = $this->fileResize($imageSourceId,$sourceProperty[0],$sourceProperty[1]);
+
+				/*second parameter is a directory*/
+				imagejpeg($targetLayer,$this->_pathUpload);
+			}
+			elseif( $image_type == IMAGETYPE_GIF )  
+			{  
+				$imageSourceId = imagecreatefromgif($fileTmp);
+
+				$targetLayer = $this->fileResize($imageSourceId,$sourceProperty[0],$sourceProperty[1]);
+
+				imagegif($targetLayer, $this->_pathUpload);
+			}
+			elseif( $image_type == IMAGETYPE_PNG )
 			{
-				if(move_uploaded_file($this->tmp_name,$this->_dir.'/'.$newName))
+				$imageSourceId = imagecreatefrompng($fileTmp); 
+
+				$targetLayer = $this->fileResize($imageSourceId,$sourceProperty[0],$sourceProperty[1]);
+
+				imagepng($targetLayer, $this->_pathUpload);
+			}
+		}
+
+		/*
+		*IMPORTANT!
+		*@param $targetDimension will be the new image dimension 
+		if you are planning to alter the default 200 200 use this following format
+		array(200,200) width , height
+		*/
+		private function fileResize($imageSourceId,$width,$height, $targetDimension = null) 
+		{
+
+			if($targetDimension == null) {
+
+				$targetWidth = 400;
+				$targetHeight = 640;
+
+				$ratio = $width / $height; //original file ratio;
+
+				if($targetWidth / $targetHeight > $ratio)
 				{
-					return true;
+					$targetWidth = $width * $ratio;
+					$targetHeight = $h;
 				}else{
-					$this->addErrors('THE FILE IS NOT UPLOADED');
-					return false;
+					$targetHeight = $height * $ratio;
+					$targetWidth  = $width;
 				}
-			}catch(Exception $e)
-			{
-				echo $e->getMessage();
-				die();
+
+			}else{
+
+				list($targetWidth , $targetHeight) = $targetDimension;
 			}
-			
 
+			$targetLayer  = imagecreatetruecolor($targetWidth,$targetHeight);
+
+			imagecopyresampled($targetLayer,$imageSourceId,0,0,0,0,$targetWidth,$targetHeight, $width,$height);
+
+			return $targetLayer;
 		}
 
-		private function generateName()
-		{
-			return $this->newname = strtolower($this->_prefix.''.random_gen().'.'.$this->ext);
-		}
 
-		private function addErrors($err)
+		public function getSize()
 		{
-			array_push($this->_errors, $err);
-		}
-
-		public function errors()
-		{
-			return implode(',',$this->_errors);
+			return $this->_fileSize;
 		}
 	}
+?>
